1131249f8358f114281b040019b0b3da24385601
Ndalama Matika <ndalama.matika@psg.co.za>
2024-06-25 15:28:23 +0200

    Day 3

--- Day3/Day3/Program.cs
+++ Day3/Day3/Program.cs
@@ -1,15 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace Day3
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        string[] schematic = File.ReadAllLines("input.txt");
+
+        int rows = schematic.Length;
+        int cols = schematic[0].Length;
+        int sum = 0;
+
+        // Define the possible movements to check adjacent cells including diagonals
+        int[] dRow = { -1, -1, -1, 0, 0, 1, 1, 1 };
+        int[] dCol = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+        for (int r = 0; r < rows; r++)
         {
+            for (int c = 0; c < cols; c++)
+            {
+                if (char.IsDigit(schematic[r][c]))
+                {
+                    int number = schematic[r][c] - '0';
+                    bool isPartNumber = false;
+
+                    // Check all adjacent cells
+                    for (int i = 0; i < 8; i++)
+                    {
+                        int newRow = r + dRow[i];
+                        int newCol = c + dCol[i];
+
+                        if (IsValid(newRow, newCol, rows, cols) && IsSymbol(schematic[newRow][newCol]))
+                        {
+                            isPartNumber = true;
+                            break;
+                        }
+                    }
+
+                    if (isPartNumber)
+                    {
+                        sum += number;
+                    }
+                }
+            }
         }
+
+        Console.WriteLine("The sum of all part numbers is: " + sum);
+    }
+
+    static bool IsValid(int r, int c, int rows, int cols)
+    {
+        return r >= 0 && r < rows && c >= 0 && c < cols;
+    }
+
+    static bool IsSymbol(char ch)
+    {
+        return !char.IsDigit(ch) && ch != '.';
     }
 }
